3073cd19939eed25246312a30d83bf3a9a06c36c
Nicolas Mora Bowen <nmorabowen@gmail.com>
2024-08-21 21:01:37 -0400

    add echos for wapeteo

--- runnn.sh
+++ runnn.sh
@@ -19,8 +19,11 @@ while [ -d "$NEW_FOLDER" ]; do
   echo "1) Remove the existing folder"
   echo "2) Enter a new name"
   echo "3) Stop the script"
+  echo
+
   read -r OPTION
-  
+  echo
+
   case $OPTION in
     1)
       rm -rf "$NEW_FOLDER"
@@ -41,11 +44,14 @@ while [ -d "$NEW_FOLDER" ]; do
   esac
 done
 
+echo
+
 # Step 1: Copy the entire folder from shared to the user's directory
 if cp -r "$BASE_FOLDER" "$NEW_FOLDER"; then
   echo "Folder copied successfully."
 else
   echo "Failed to copy folder. Exiting script."
+  echo
   exit 1
 fi
 
@@ -54,14 +60,17 @@ if cp "$SCRIPT_FILE" "$NEW_FOLDER"; then
   echo "Script file copied successfully."
 else
   echo "Failed to copy script file. Exiting script."
+  echo
   exit 1
 fi
 
 # Step 3: Replace 'nmbTEMP' with the new folder name in the run.sh file
 if sed -i "s/nmbTEMP/$NEW_FOLDER_NAME/g" "$NEW_FOLDER/run.sh"; then
   echo "Job name updated successfully in run.sh."
+  echo
 else
   echo "Failed to update job name in run.sh. Exiting script."
+  echo
   exit 1
 fi
 
@@ -69,12 +78,14 @@ fi
 cd "$NEW_FOLDER" || { echo "Failed to change directory to $NEW_FOLDER. Exiting script."; exit 1; }
 if sbatch run.sh; then
   echo "Job submitted successfully."
+  echo
 else
   echo "Failed to submit job. Exiting script."
+  echo
   exit 1
 fi
 
-echo "/n"
+echo 
 echo "------------------------------"
 echo "LARGA VIDA AL LADRUÑO!"
 echo "------------------------------"
