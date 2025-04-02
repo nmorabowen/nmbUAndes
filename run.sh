54c5f2c8f78c304639175fe30e5177c9b44ffa8a
Nicolas Mora Bowen <nmorabowen@gmail.com>
2025-04-01 23:31:15 -0300

    change opensees

--- run.sh
+++ run.sh
@@ -49,7 +49,7 @@ SECONDS=0
 
 if [[ "$MODE" == "slurm" ]]; then
   # SLURM execution
-  mpirun /mnt/nfshare/bin/openseesmp-16102024 main.tcl
+  mpirun /mnt/nfshare/bin/opensees-10012025 main.tcl
 else
   # Local execution
   for ((node=1; node<=NODES; node++)); do
@@ -59,7 +59,7 @@ else
     done
   done
   # Run OpenSees locally
-  /mnt/nfshare/bin/openseesmp-16102024 main.tcl
+  /mnt/nfshare/bin/opensees-10012025 main.tcl
 fi
 
 # Log completion
