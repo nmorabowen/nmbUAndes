2327ff3f4787b5cdc8650e085da5dc48f2d0cbf2
Nicolas Mora Bowen <nmorabowen@gmail.com>
2025-04-16 21:57:32 -0400

    node17 and 18

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --exclude=node18
+#SBATCH --exclude=node17,node18
 #SBATCH --job-name=nmbTEMP
 #SBATCH --ntasks=8                    # Valor por defecto si no se pasa desde afuera
 #SBATCH --output=log.log
